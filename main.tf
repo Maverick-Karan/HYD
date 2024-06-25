9a33b12707bb6d3dee6cd01a5231827e8afc168c
Karan <karan_chgh@yahoo.com>
2024-06-25 18:26:12 -0400

    changed module name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ## create new account in VPC
 module "new_account" {
-  source                  = "./modules/new_account"
+  source                  = "./modules/account"
   name                    = var.name
   email                   = var.email
   role_name               = var.role_name
