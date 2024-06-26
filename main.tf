f436e35befe0471834ca9ab456e00d699475d69f
Karan <karan_chgh@yahoo.com>
2024-06-25 22:26:09 -0400

    added provider

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@
 #  role_name               = var.role_name
 #}
 
-#provider "aws" {
-#  alias   = "master"
-#  region  = "us-east-1"
-#}
+provider "aws" {
+  alias   = "master"
+  region  = "us-east-1"
+}
 
 resource "aws_organizations_account" "new_account" {
   provider = aws.master
